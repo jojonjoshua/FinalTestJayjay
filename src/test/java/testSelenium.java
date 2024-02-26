43d2f86e40c423759f266c9d42e7cfc4ebd3fc78
jojonjoshua <133229159+jojonjoshua@users.noreply.github.com>
2024-02-27 00:31:01 +0700

    FinalTest Commit 3

---
+++ src/test/java/testSelenium.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class testSelenium {
+
+    public static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-she-usage");
+        options.addArguments("--remote-allow-origins");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://pintu.co.id");
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+}
