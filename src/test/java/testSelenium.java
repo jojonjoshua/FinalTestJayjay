66e9c04f724e62448bd4dc45ab773b5ea2ffc159
DaffaPintu <daffa@pintu.co.id>
2024-02-27 20:01:10 +0700

    FIX

--- src/test/java/testSelenium.java
+++ src/test/java/testSelenium.java
@@ -1,4 +1,3 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,15 +11,14 @@ public class testSelenium {
         ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-she-usage");
-        options.addArguments("--remote-allow-origins");
-        WebDriverManager.chromedriver().setup();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://pintu.co.id");
         Thread.sleep(5000);
         driver.quit();
-
     }
 }
